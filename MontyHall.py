b7754807b766ad4c12d9a92cae1bb2a3c4d91eab
MitchellNix <mnix00@gmail.com>
2017-12-16 12:53:17 -0600

    Update MontyHall.py

--- MontyHall.py
+++ MontyHall.py
@@ -5,8 +5,8 @@
 ##of a set of 3 boxes and the user is asked for a box selection
 ##does the probability of the user having selected the correct box
 ##remain the same, or increase, when the user is given the option
-##to switch choices after the box not containing the object is removed
-##from the selection pool
+##to switch choices after the box not containing the object has been
+##removed from the selection pool
 
 import random
 
